91f02ef0aef87dfd39121c85a25c1b4f258b4833
caicancan <357199245@qq.com>
2018-04-18 21:49:28 +0800

    build

--- app/src/main/java/com/example/thirdlogin/MainActivity.java
+++ app/src/main/java/com/example/thirdlogin/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         //传入参数APPID和全局Context上下文
 
